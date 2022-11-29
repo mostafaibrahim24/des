7be4261621a152af1eb768ebe7ca24d18be1a86f
mostafaibrahim24 <mi.mostafaibrahim@gmail.com>
2022-11-29 17:20:24 +0200

    Add plaintext input handling

--- des.py
+++ des.py
@@ -1,3 +1,4 @@
+import textwrap
 
 
 
@@ -13,8 +14,23 @@
 
 
 
-
-
+def formatPlaintext(plaintext):
+    if(len(plaintext)<8):
+        for i in range(0,8-len(plaintext)):
+            plaintext+="#"
+    elif(len(plaintext)>8):
+        plaintext = textwrap.wrap(plaintext,8)
+        return plaintext,True
+    return plaintext,False
+def changeToBits(plaintext,isLargerThan8Chars):
+    if(isLargerThan8Chars):
+        for i in range(0,len(plaintext)):
+            plaintext[i]=format(int.from_bytes(plaintext[i].encode(),'big'),'064b')
+        return plaintext
+    plaintext=format(int.from_bytes(plaintext.encode(),'big'),'064b')
+    
 
 if __name__ == '__main__':
-    print("")
+    plaintext = input("Enter plain text: ")
+    plaintext,isLargerThan8Chars = formatPlaintext(plaintext)
+    plaintext=changeToBits(plaintext,isLargerThan8Chars)
